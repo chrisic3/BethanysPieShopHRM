b41670e1d00cc8f8140bf8dbcae036631ec78b7b
chrisic3 <chris_ic3@hotmail.com>
2025-04-23 20:53:22 +0000

    Add Developer class

---
+++ BethanysPieShopHRM/HR/Developer.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace BethanysPieShopHRM.HR;
+
+public class Developer : Employee
+{
+    private string currentProject;
+
+    public string CurrentProject
+    {
+        get
+        {
+            return currentProject;
+        }
+
+        set
+        {
+            currentProject = value;
+        }
+    }
+
+    public Developer(string firstName, string lastName, string email, 
+        DateTime birthday, double? hourlyRate) : base(firstName, lastName, 
+        email, birthday, hourlyRate)
+    {
+
+    }
+}
